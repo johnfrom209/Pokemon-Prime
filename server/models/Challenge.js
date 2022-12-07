72f8d6dd99de646d40ecf1e915c54a0718a3f969
chanjeff520 <chanjeff520@gmail.com>
2022-12-07 05:59:24 -0500

    add ChallengeSchema

--- server/models/Challenge.js
+++ server/models/Challenge.js
@@ -1,1 +1,17 @@
 const {Schema, model} = require('mongoose');
+const Game = require('./Game');
+const User = require('./User');
+const Pokemon = require('./Pokemon');
+
+const ChallengeSchema = new Schema({
+    game: Game,
+    player1: User,
+    player2: User,
+    battleParty: [Pokemon],
+    battleParty2: [Pokemon],
+    p1Caught: [Pokemon],
+    p2Caught: [Pokemon],
+});
+
+const Challenge = model('Challenge', ChallengeSchema);
+module.exports = Challenge;
