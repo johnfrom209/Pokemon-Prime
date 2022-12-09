8e6c301b964818f4a37e6dba8ba37406ef694c4c
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-09 01:53:29 -0800

    added a bit more style, but its still a mess

--- pokemon-prime/src/components/challenge/AddPokemon.js
+++ pokemon-prime/src/components/challenge/AddPokemon.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function AddPokemon({ pokemon }) {
     return (
-        <div className='h-fit w-full dragThis bg-indigo-300 rounded mb-2'>
+        <div className='h-fit w-full dragThis bg-indigo-300 rounded mb-2 text-sm'>
             <div className='p-5 flex space-x-3 pb-0'>
-                Name: {pokemon.name}
+                <span className='italic mr-2'>
+
+                    Name: {pokemon.name}
+                </span>
                 Species: {pokemon.species}
                 Type: {pokemon.type}
 
-                <img src={pokemon.sprite} alt={`Pokemon. Nicknamed ${pokemon.name}`} className="h-14 block m-auto" />
-            </div>
-            <div className='flex'>
-
+                <img src={pokemon.sprite} alt={`Pokemon. Nicknamed ${pokemon.name}`} className="h-14 " />
             </div>
         </div>
     )
