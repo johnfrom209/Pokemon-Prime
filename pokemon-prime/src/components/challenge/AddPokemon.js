415d2ad6f02ca6ab413a71084a4db8e4ebf6bdf0
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-09 00:59:15 -0800

    added a form to the challenge page, added styling to the AddPokemon file

--- pokemon-prime/src/components/challenge/AddPokemon.js
+++ pokemon-prime/src/components/challenge/AddPokemon.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function AddPokemon({ pokemon }) {
     return (
-        <div className='h-fit w-full dragThis bg-indigo-300 rounded'>
-            <div className='p-5'>
-                Name: {pokemon.name} <br />
-                Species: {pokemon.species} <br />
+        <div className='h-fit w-full dragThis bg-indigo-300 rounded mb-2'>
+            <div className='p-5 flex space-x-3 pb-0'>
+                Name: {pokemon.name}
+                Species: {pokemon.species}
                 Type: {pokemon.type}
 
+                <img src={pokemon.sprite} alt={`Pokemon. Nicknamed ${pokemon.name}`} className="h-14 block m-auto" />
             </div>
-            <div className='relative'>
-                <img src={pokemon.sprite} alt={`Pokemon. Nicknamed ${pokemon.name}`} className="h-1/5 " />
+            <div className='flex'>
 
             </div>
         </div>
--- pokemon-prime/src/components/challenge/challenge.js
+++ pokemon-prime/src/components/challenge/challenge.js
@@ -18,6 +18,8 @@ var drake = dragula([document.querySelector('.player1Caught'), document.querySel
         return el.classList.contains('dragula-container');
     }
 });
+
+
 // this is temp
 const spriteList = [
     {
@@ -42,6 +44,8 @@ export default function Challenge() {
     const [battleparty, setBattleparty] = useState([]);
     const [graveyard, setGraveyard] = useState([]);
     const [player1Caught, setPlayer1Caught] = useState(spriteList);
+    const [addPokemon, setAddPokemon] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     // useEffect(() => {
     //     console.log('battleparty', battleparty);
@@ -50,6 +54,36 @@ export default function Challenge() {
     const renderPlayer1Caught = player1Caught.map((pokemon) => {
         return <AddPokemon key={pokemon.id} pokemon={pokemon} />
     })
+    //I don't feel like I need this. I can just use the pokemon name from the input
+    // const handleInputChange = (e) => {
+    //     const { value } = e.target;
+    //     //what is value here?
+    //     console.log(value);
+
+    //     const pokemonName = value.trim();
+
+    //     if (pokemonName.length) {
+    //         // setAddPokemon(pokemonName);
+    //     } else {
+    //         setAddPokemon('');
+    //     }
+    // }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if (!addPokemon) {
+            setErrorMessage('Please enter a Pokemon name');
+            return;
+        }
+
+        //call api to get pokemon info
+        //add pokemon to player1Caught
+        //setAddPokemon('');
+
+        setAddPokemon('');
+        setErrorMessage('');
+
+    }
 
     return (
         <div className='grid grid-cols-3'>
@@ -61,19 +95,36 @@ export default function Challenge() {
                 {/* added class 'player1Caught' just for identification. it does nothing */}
                 <div className='bg-indigo-800 h-4/5 m-5 rounded' >
                     {/* this is the contain for the Pokemon they caught */}
-                    <div className='player1Caught dragula-container h-full w-full '>
+                    <div className='player1Caught dragula-container h-full w-full overflow-auto'>
                         {renderPlayer1Caught}
                     </div>
                 </div>
             </div>
             <div className='h-screen bg-gray-500 '>
-                <div className='bg-gray-800 h-1/4 my-2 mt-24 rounded'>
+                <div className='bg-gray-800 h-1/4 my-2 mt-24 rounded relative'>
                     {/* add pokemon */}
+                    {errorMessage && (
+                        <div className='pt-5'>
+                            <p className="error-text text-white text-center">{errorMessage}</p>
+                        </div>
+                    )}
+                    <form className='absolute bottom-0 right-20'>
+                        <input
+                            type='text'
+                            placeholder='Pokemon Name'
+                            className='w-1/2 p-2 rounded'
+                        />
+                        <button
+                            className='bg-gray-300 p-2 m-2 rounded'
+                            onClick={handleFormSubmit}
+                        >Add Pokemon</button>
+                    </form>
 
                 </div>
+
                 <div className='bg-gray-800 h-1/4 my- rounded2'>
                     {/* Battle Party */}
-                    <div className='battleparty dragula-container h-full w-full flex'>
+                    <div className='battleparty dragula-container h-full w-full grid grid-cols-6 '>
 
                     </div>
 
