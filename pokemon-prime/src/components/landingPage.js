5c04b2a3a73def4e904118ca2d7a00dfa794197a
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-12 19:31:49 -0600

    added code to LandingPage.js

--- pokemon-prime/src/components/landingPage.js
+++ pokemon-prime/src/components/landingPage.js
@@ -1,1 +1,29 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Login from "./Login";
+import signin from "./SignUp";
+import Rules from "./Rules";
+import NewChallenge from "./challenge/Challenge";
+//landing page
+export default function landingPage() {
+  const [currentPage, setCurrentPage] = useState("Home");
 
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "Rules") {
+      return <Rules />;
+    }
+    if (currentPage === "New Challenge") {
+      return <NewChallenge />;
+    }
+    if (currentPage === "Sign In") {
+      return <Login />;
+    }
+    if (currentPage === "Logout") {
+      return <Logout />;
+    }
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
+}
