d4dd814e5502152056aecf4838fe8a4f1dc1823a
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-12 19:33:43 -0600

    added rules to Rules.js

--- pokemon-prime/src/components/rules.js
+++ pokemon-prime/src/components/rules.js
@@ -1,2 +1,19 @@
-
-
+import React from "react";
+//rules page
+export default function Rules() {
+  return (
+    <div>
+      <h1>Rules</h1>
+      <ul>
+        <li>
+          1. You are only able to catch the first wild Pokemon encountered in
+          each area
+        </li>
+        <li>2. you must name each Pokemon you catch</li>
+        <li>3. If your Pokemon faints it's considered "dead"</li>
+        <li>4. Your Pokemon can not be above the gym leader's Pokemon level</li>
+        <li>5. You can not have duplicate species of Pokemon</li>
+      </ul>
+    </div>
+  );
+}
