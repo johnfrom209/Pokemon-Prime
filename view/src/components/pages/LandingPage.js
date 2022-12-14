bb1f65caa63ae53d805403dd5d93555b09f8016c
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-14 17:59:25 -0600

    just need background color for landing page

--- view/src/components/pages/LandingPage.js
+++ view/src/components/pages/LandingPage.js
@@ -1,12 +1,24 @@
 import React from "react";
+import PPlogo from "../../images/PP.PNG";
+import newChallenge from "../challenge/Challenge";
 
 export default function LandingPage() {
   return (
-    <div>
-      <img src="../../images/PP.PNG" alt="PP logo"></img>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Start Nuzlocke
-      </button>
+    <div className="self-center w-full max-w-sm p-4 bg-blue-400 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <img className="rounded-t-lg" src={PPlogo} alt="logo" />
+
+      <div className=" p-5">
+        <p className="mb-3 font-normal text-center text-gray-700 dark:text-gray-400">
+          Welcome to Pokemon Prime
+        </p>
+
+        <button
+          href={newChallenge}
+          className="ml-10 inline-flex px-6 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Start Nuzlocke
+        </button>
+      </div>
     </div>
   );
 }
