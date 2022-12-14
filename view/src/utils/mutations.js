3f6aaa763aebdd35428ea38b4503fc06dd77209a
chanjeff520 <chanjeff520@gmail.com>
2022-12-14 01:54:35 -0500

    add addUser and login to mutation in client

--- view/src/utils/mutations.js
+++ view/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 //add pokemon to database
 //example: {
@@ -11,28 +11,70 @@ import { gql } from '@apollo/client';
 //     "evolution": ["Raichu"]
 //}
 
-export const Mutation_AddPokemon = gql`
-mutation addPokemon($name: String!, $species: String!, $pokemonType: [String]!, $superEffective: [String]!, $weakness: [String]!, $sprite: String!, $evolution: [String]!) {
-    addPokemon(name: $name, species: $species, pokemonType: $pokemonType, superEffective: $superEffective, weakness: $weakness, sprite: $sprite, evolution: $evolution) {
+//add user to database
+export const Mutation_AddUser = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
         _id
-        name
-        species
-        pokemonType
-        superEffective
-        weakness
-        sprite
-        evolution
+        username
+      }
     }
-}
+  }
+`;
+
+//login user
+export const Mutation_LoginUser = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const Mutation_AddPokemon = gql`
+  mutation addPokemon(
+    $name: String!
+    $species: String!
+    $pokemonType: [String]!
+    $superEffective: [String]!
+    $weakness: [String]!
+    $sprite: String!
+    $evolution: [String]!
+  ) {
+    addPokemon(
+      name: $name
+      species: $species
+      pokemonType: $pokemonType
+      superEffective: $superEffective
+      weakness: $weakness
+      sprite: $sprite
+      evolution: $evolution
+    ) {
+      _id
+      name
+      species
+      pokemonType
+      superEffective
+      weakness
+      sprite
+      evolution
+    }
+  }
 `;
 
 //need to add the mutation to add to player's caught pokemon
 export const Mutation_AddPlayer1Caught = gql`
-mutation addPlayer1Caught($challengeId: ID!, $pokemonId: ID!) {
+  mutation addPlayer1Caught($challengeId: ID!, $pokemonId: ID!) {
     addPlayer1Caught(challengeId: $challengeId, pokemonId: $pokemonId) {
-        challenge {
-            player1Caught
-        }
+      challenge {
+        player1Caught
+      }
     }
-}
+  }
 `;
