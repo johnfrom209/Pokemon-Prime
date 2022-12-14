ecc6cc0eb2229f6d830cc5749a753e83d9dd558f
robogf <111620315+robogf@users.noreply.github.com>
2022-12-13 18:34:24 -0800

    added a sample card for a skeleton for our profile card, and added our custom colors in our tailwind config for use

--- view/tailwind.config.js
+++ view/tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,tx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'mainColor': '#066A9B',
+        'SapPC0': '#0A2672',
+        'SapPC1': '#030A1C',
+        'SapPC2': '#06184A',
+        'SapPC3': '#0B3199',
+        'SapPC4': '#0742E4',
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'RubPC0': '#830000',
+        'RubPC1': '#1E0202',
+        'RubPC2': '#3D0000',
+        'RubPC3': '#BC0000',
+        'RubPC4': '#FF0000',
+    }
+
   },
   plugins: [],
 }
