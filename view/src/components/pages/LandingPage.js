22ee9c869d2f9bb415b3999d17ac3826d3ab89e1
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-14 17:01:56 -0600

    styling landing page

--- view/src/components/pages/LandingPage.js
+++ view/src/components/pages/LandingPage.js
@@ -1,11 +1,12 @@
-
-import React from 'react';
+import React from "react";
 
 export default function LandingPage() {
-    return(
-        <div>
-            <h1>Landing Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <img src="../../images/PP.PNG" alt="PP logo"></img>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Start Nuzlocke
+      </button>
+    </div>
+  );
 }
-
