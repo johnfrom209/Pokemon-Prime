6927950da961d9dfee09bd7ae0d7e09a1ff32386
robogf <111620315+robogf@users.noreply.github.com>
2022-12-14 23:47:34 -0800

    added local storage of player 1's caught pokemon to add to our profile status

--- view/src/components/pages/Profile.js
+++ view/src/components/pages/Profile.js
@@ -1,27 +1,27 @@
 // profile page 
-import React from "react";
+import React,{useState} from "react";
 // import {useParams} from "react-router-dom";
 import trainer from "../../images/Brendan.png";
 import textbg from "../../images/pokemon-text.png";
 import trainerF from "../../images/May.png";
 import Auth from "../../utils/auth";
-// import { useQuery } from "@apollo/client";
-// import {QUERY_USER}  from "../../utils/queries";
 
 const Profile = () => {
   const userData = Auth.getProfile().data.username;
-
   console.log(userData)
-//   const {username: userParam} = useParams();
-//   const {data} = useQuery(userParam ? QUERY_USER : QUERY_USER,{
-//     variables: {username: userParam}
-//   });
-// if(Auth.getProfile().data.username === userParam){
-//   console.log("true")
-// }
-  // const user = data?.username || {};
-  // console.log(user);
-    return (
+//   let p1name = "Player 1";
+// let pokemonArray = localStorage.getItem("player1Caught", JSON.stringify());
+let spriteList = JSON.parse(localStorage.getItem("player1Caught",)) ?? [];
+
+
+
+// const [player1Caught, setPlayer1Caught] = useState(spriteList);
+// const renderPlayer1Caught = player1Caught.map((pokemon) => {
+//   return <AddPokemon key={pokemon.id} pokemon={pokemon} />
+// })
+
+
+  return (
       
       
         <div className="bg-white-50 w-screen flex flex-col justify-center items-center"  style={{ backgroundImage: `url(${textbg})`, backgroundRepeat:"no-repeat", backgroundSize:"contain", backgroundPosition:"center", backgroundColor:"white"}}>
@@ -34,7 +34,7 @@ const Profile = () => {
                 
               <div className="text-gray-900 font-bold text-xl mb-2 text-center ">Trainer Card</div>
               <h1 className="text-gray-700 text-center text-base"> Here Are Your Stats For Your Challenge:</h1>
-                <h2 className="text-gray-700 text-center text-base">Pokemon Caught:</h2>
+                <h2 className="text-gray-700 text-center text-base">Pokemon Caught:{spriteList.length}</h2>
                 <h2 className="text-gray-700 text-center text-base">Wins:</h2>
                 <h2 className="text-gray-700 text-center text-base">Losses:</h2>
                 <h2 className= "text-gray-700 text-center text-base">Username:{`${userData}`}</h2>
