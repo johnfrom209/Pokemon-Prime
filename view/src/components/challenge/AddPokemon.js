8a67d6c629dbbcf45bd009f024c5166fcc8ff7e7
Jonathan Moreno <johnfrom209@gmail.com>
2023-05-02 02:03:52 -0700

    Added a Function to capitalize the first letter in a word.

--- view/src/components/challenge/AddPokemon.js
+++ view/src/components/challenge/AddPokemon.js
@@ -3,13 +3,21 @@ import PokemonType from './PokemonType'
 
 
 export default function AddPokemon({ pokemon }) {
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    pokemon.name = capitalizeFirstLetter(pokemon.name);
+    pokemon.species = capitalizeFirstLetter(pokemon.species);
+
     return (
         <div className='card-info dragThis bg-sapp-400 rounded mb-4  xl:w-40 xl:h-48 h-40 w-32 ml-1 border-4 border-amber-300 truncate '>
             <div className='py-3 px-5 relative space-x-1 pb-0 h-16 '>
                 <span className='italic mr-2 text-xs xl:textSize'>
                     {pokemon.name}
                 </span>
-                <img src={pokemon.sprite} alt={`Pokemon Nicknamed ${pokemon.name} `} className="h-8 xl:h-12 my-4 block pl-6" />
+                <img src={pokemon.sprite} alt={`Pokemon Nicknamed ${pokemon.name} `} className="h-8 xl:h-12 my-4 block pl-6 " />
                 <span className="text-xs"
                 >
                     {pokemon.species}
